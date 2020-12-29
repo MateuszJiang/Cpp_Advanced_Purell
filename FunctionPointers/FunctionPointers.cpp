493fa568f99849732efb1077a72f26ca8e47bd0a
Matthew Caine <mateuszczylok1997@gmail.com>
2020-12-29 17:40:11 +0000

    extra fn pointer exercise

--- FunctionPointers/FunctionPointers.cpp
+++ FunctionPointers/FunctionPointers.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-void test(int value)
+void print_message(int value)
 {
     std::cout << "Hello " << value << std::endl;
 }
 
+bool match(std::string test)
+{
+    return test.size() == 5;
+}
+
+int count_strings_of_five(std::vector<std::string>& texts, bool (*matching_string)(std::string test))
+{
+    int counter = 0;
+    for (int i = 0; i < texts.size(); i++)
+    {
+        if (match(texts[i]))
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+
 int main()
 {
-    test(7);
+    print_message(7);
 
-    void (*pTest)(int) = test;
-    pTest = test; 
+    void (*p_print_message)(int) = print_message;
+    p_print_message = print_message;
 
-    pTest(8);
+    p_print_message(8);
+
+
+    ///////////////////////////////////////////////////////////////////////
+
+
+    std::vector<std::string> texts;
+    texts.push_back("aaaaa");
+    texts.push_back("aaaaa");
+    texts.push_back("aaa");
+    texts.push_back("aaaaa");
+    texts.push_back("aaaaaaa");
+
+    std::cout << std::count_if(texts.begin(), texts.end(), match) << std::endl;
+
+    std::cout << count_strings_of_five(texts, match);
 }
 
